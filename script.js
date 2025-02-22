0aa29ad9baa0c5def68cef9e65bd804c58a66991
Arvind Singh <145861795+04arvind@users.noreply.github.com>
2025-02-22 20:18:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let typingEffect = new Typed("#text",{
+    strings:["Arvind Singh","Passionate Software Engineer", "Full stack Developer"],
+    loop:true,
+    typeSpeed:100,
+    backSpeed:50,
+    backDelay:1000
+})
